bd6d466036ac4eae3ea5b4bba7f3c5ec4e843920
Zachary Bryce Ellis <zellis6@hotmail.com>
2021-05-28 23:11:38 -0500

    Update ViewController.swift

--- Happiness Tracker/ViewController.swift
+++ Happiness Tracker/ViewController.swift
@@ -13,7 +13,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+/*   Label that will display the Date and it's function
+     
+     
+     
+     @IBOutlet weak var uiLabel: UILabel!
+     
+     @IBAction func getDateAndTime(Sender: UIButton){
+             let dateFormatter = DateFormatter();
+             dateFormatter.dateFormat = "yyyy-MM-dd hh:mm::ss"
+             
+             let date = dateFormatter.string(from: Date())
+             
+             self.uiLabel.text = date
+         }
+     
+     https://programmerworld.co/ios/how-to-get-current-date-and-time-in-your-ios-app-xcode-tutorial/
+     
+     
+     */
+    
+    
+
+
 
 
 }
+
 
